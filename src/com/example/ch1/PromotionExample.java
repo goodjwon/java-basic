ddee895cc09693e2b8acfb0452d184f76c25c897
JungWon Park <firstman9@protonmail.com>
2024-10-21 21:10:56 +0900

    first commit

---
+++ src/com/example/ch1/PromotionExample.java
@@ -0,0 +1,9 @@
+package com.example.ch1;
+
+public class PromotionExample {
+    public static void main(String[] args) {
+        byte byteValue = 10;
+        int intValue = byteValue; // 자동 타입 변환
+        System.out.println("intValue: " + intValue);
+    }
+}
---
+++ src/com/example/ch1/VariableExample.java
@@ -0,0 +1,14 @@
+package com.example.ch1;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        // 변수 선언
+        int age;
+        // 변수 초기화
+        age = 24;
+        // 선언과 동시에 초기화
+        double value = 40.8;
+        System.out.println("Age: " + age);
+        System.out.println("Value: " + value);
+    }
+}
